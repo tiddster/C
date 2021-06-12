16cc422c5830f432afc8fbc679742fe09d27fdcb
tiddster <864187149@qq.com>
2021-06-12 11:03:37 +0800

    6.12

--- oj/链表.cpp
+++ oj/链表.cpp
@@ -7,7 +7,7 @@ typedef struct data{
 
 DATA *CreateList(int num);   //创建 
 DATA *FindList(DATA *head, int index);   //查找
-DATA *FindLast(DATA *head);  //查找尾部 
+DATA *FindLast(DATA *head, int last);  //查找尾部 
 DATA *Insert(DATA *head, int index); //插入 
 DATA *Delete(DATA *head, int index); //删除 
 
@@ -16,7 +16,10 @@ int main(void) {
 	scanf("%d",&n);
 	DATA *head;
 	head = CreateList(n);
-	head = FindList(head,2);
+	/*
+	head = FindList(head,2);        查找第2个 
+	head = FindLast(head,n);		查找第n个~最后一个 
+	*/
 	printf("%d",head->dta);
 }
 
@@ -49,6 +52,14 @@ DATA *CreateList(int num){
 DATA *FindList(DATA *head, int index){
 	DATA *D = head;
 	for(int i=1; i<index && D!=NULL; i++){
+		D = D->next;
+	}
+	return D;
+} 
+
+DATA *FindLast(DATA *head, int last){
+	DATA *D = head;
+	for(int i=1; i<last && D!=NULL; i++){
 		D = D->next;
 	}
 	return D;
