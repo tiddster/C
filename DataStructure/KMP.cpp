5727f6f5261e28ee5f7ae2f93833bab733d02d07
tiddster <864187149@qq.com>
2021-08-30 18:04:42 +0800

    8.30

--- DataStructure/KMP.cpp
+++ DataStructure/KMP.cpp
@@ -1,5 +1,31 @@
 #include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
 
-void get_next(char[] T, int *next){
-	
-} 
+void get_next(char *T, int *next);
+
+int main(void){
+	char *T = "aabaaf";
+	int next[strlen(T)];
+	get_next(T, next);
+	for(int i=1; i<=strlen(T); i++){
+		printf("%d\n", *(next+i));
+	}
+}
+
+void get_next(char *T, int* next){
+	int i = 0, j = 1;
+	next[1] = 0;
+	while(j<=strlen(T)){
+		if(T[i] == T[j]){
+			i++;
+			j++;
+			next[j] = i;
+		} else if(i == 0) {
+			j++;
+			next[j] = 0;
+		} else {
+			i = next[i];
+		}
+	}
+}
