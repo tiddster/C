e90274e45b4f554afb728d653f89e6f6fa9964e2
tiddster <864187149@qq.com>
2021-08-31 18:06:21 +0800

    8.31

--- DataStructure/KMP.cpp
+++ DataStructure/KMP.cpp
@@ -1,16 +1,16 @@
-#include<stdio.h>
+ #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
 
 void get_next(char *T, int *next);
+void match(char *S, char *T, int *next); 
 
 int main(void){
 	char *T = "aabaaf";
+	char *S = "aabaabaaf";
 	int next[strlen(T)];
 	get_next(T, next);
-	for(int i=1; i<=strlen(T); i++){
-		printf("%d\n", *(next+i));
-	}
+	match(S, T, next);
 }
 
 void get_next(char *T, int* next){
@@ -26,6 +26,29 @@ void get_next(char *T, int* next){
 			next[j] = 0;
 		} else {
 			i = next[i];
+		}
+	}
+}
+
+void match(char *S, char *T, int *next){
+	if(strlen(S)<strlen(T)) return;
+	
+	int i=0, j=0;
+	
+	while(i<strlen(S)){
+		if(S[i] == T[j]){
+			i++;
+			j++;
+			if(j==strlen(T)) {
+				printf("success");
+				return;
+			}
+		} else {
+			if(i==strlen(S)-1){
+				printf("failed");
+				return;
+			}
+			j = next[j];
 		}
 	}
 }
